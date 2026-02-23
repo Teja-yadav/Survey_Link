02816e0841a6f5486c454164b2ce625b0a4cfebc
Teja-yadav <dummy@gmail.com>
2026-02-23 17:05:23 +0000

    Day4 task

--- server/src/main/java/com/edutech/progressive/entity/Warehouse.java
+++ server/src/main/java/com/edutech/progressive/entity/Warehouse.java
@@ -1,6 +1,12 @@
 package com.edutech.progressive.entity;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Warehouse implements Comparable<Warehouse>{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int warehouseId;
     private int supplierId;
     private String warehouseName;
